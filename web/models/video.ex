dd03c0dfe7699675c6b9309ebc7fbaf5cf4b798f
Jeremy Reyes <jreyes@merkleinc.com>
2017-03-21 23:20:22 -0700

    Validating Foreign Keys

--- web/models/video.ex
+++ web/models/video.ex
@@ -18,7 +18,8 @@ defmodule Rumbl.Video do
   @optional_fields ~w(category_id)a
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, @required_fields)
+    |> cast(params, @required_fields ++ @optional_fields)
     |> validate_required(@required_fields)
+    |> assoc_constraint(:category)
   end
 end
