c90c70f4a4eca77530d10bcb33c4eff005f68dab
Jeremy Reyes <jreyes@merkleinc.com>
2017-05-23 23:11:00 -0700

    Isolating Wolfram

--- apps/rumbl/test/test_helper.exs
+++ apps/rumbl/test/test_helper.exs
@@ -1,4 +1,7 @@
+Code.require_file "../../info_sys/test/backends/http_client.exs", __DIR__
 ExUnit.start
 
+Mix.Task.run "ecto.create", ~w(-r Rumbl.Repo --quiet)
+Mix.Task.run "ecto.migrate", ~w(-r Rumbl.Repo --quiet)
+# Ecto.Adapters.SQL.begin_test_transaction(Rumbl.Repo) ## produces warning (UndefinedFunctionError) function Ecto.Adapters.SQL.begin_test_transaction/1 is undefined or private.
 Ecto.Adapters.SQL.Sandbox.mode(Rumbl.Repo, :manual)
-
